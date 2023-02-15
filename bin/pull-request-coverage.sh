e94d75c11fa5d46b8e06f52bf791d2b737a2959b
dmsantosd <diego.isantos@mercadolivre.com>
2023-02-15 22:40:20 +0000

    Change repository url

--- bin/pull-request-coverage.sh
+++ bin/pull-request-coverage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Getting pull request head branch..."
-export PHPUNIT_HEAD_BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/${PR_NUMBER} \
+export PHPUNIT_HEAD_BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/payment-magento-plugin/pulls/${PR_NUMBER} \
 | jq ".head.ref" \
 | xargs)
 
 echo "Getting pull request files..."
-export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/${PR_NUMBER}/files \
+export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/payment-magento-plugin/pulls/${PR_NUMBER}/files \
 | jq ".[].filename" \
 | grep -E  'php"$' \
 | xargs)
