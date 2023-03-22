9e02cb31a5e7b36dfb00cb131a2a460569699b87
dmsantosd <diego.isantos@mercadolivre.com>
2023-03-22 21:51:24 +0000

    Chamada da Api e Congrats para pagto com 2 cartões

---
+++ Block/Order/Success/Results.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MercadoPago\PaymentMagento\Block\Order\Success;
+
+use Magento\Checkout\Block\Onepage\Success as SuccessDefault;
+
+class Results extends SuccessDefault
+{
+
+}
