a41dde873b3061969fd649d3d19aba92ac568f77
mlafernandes <ext_anlferna@mercadolivre.com>
2023-03-28 12:49:16 +0000

    Criado testes da classe ConfigPaymentMethodsOff

--- Tests/Unit/Gateway/Config/ConfigPaymentMethodsOffTest.php
+++ Tests/Unit/Gateway/Config/ConfigPaymentMethodsOffTest.php
@@ -4,12 +4,17 @@ namespace Tests\Unit\Gateway\Config;
 
 use PHPUnit\Framework\TestCase;
 
+use Magento\Framework\Stdlib\DateTime\Datetime;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\Stdlib\DateTime\DateTime;
-use Magento\Payment\Gateway\Config\Config as PaymentConfig;
+use MercadoPago\PaymentMagento\Gateway\Config\Config;
 use Magento\Store\Model\ScopeInterface;
+use MercadoPago\PaymentMagento\Gateway\Config\ConfigPaymentMethodsOff;
 
 class ConfigPaymentMethodsOffTest extends TestCase {
+    /**
+     * path pattern.
+     */
+    public const PATH_PATTERN = 'payment/%s/%s';
     /**
      * @var configPaymentMethodsOff
      */
@@ -37,21 +42,115 @@ class ConfigPaymentMethodsOffTest extends TestCase {
 
     public function setUp(): void 
     {
-        $this->scopeConfigMock = $this->getMockBuilder(ScopeConfigInterface::class)->getMockForAbstractClass();
-        $this->dateMock = $this->getMockBuilder(DateTime::class)->disableOriginalConstructor()->getMock();
+        $this->scopeConfigMock = $this->getMockBuilder(ScopeConfigInterface::class)
+            ->setMethods(['getValue'])->getMockForAbstractClass();
+
         $this->configMock = $this->getMockBuilder(Config::class)->disableOriginalConstructor()->getMock();
+        $this->dateMock = $this->getMockBuilder(DateTime::class)->disableOriginalConstructor()->getMock();
 
-        $this->configPaymentMethodsOffMock = $this->getMockBuilder(ConfigPaymentMethodsOff::class)->setMethods(
-            ['isActive'])->disableOriginalConstructor()->getMock();
-
-        //$this->configPaymentMethodsOffMock->expects($this->any())->method('isActive')->willReturn(true);
-
-        $this->configPaymentMethodsOffMock->method('isActive')->willReturn(true);
+        $this->configPaymentMethodsOffMock = $this->getMockBuilder(ConfigPaymentMethodsOff::class)
+                ->setConstructorArgs([
+                    'scopeConfig' => $this->scopeConfigMock,
+                    'date' => $this->dateMock,
+                    'config' => $this->configMock,
+                    'methodCode' => 'mercadopago_paymentmagento_payment_methods_off'
+            ])->getMock();
     }
 
-    public function test_getMethod_Active(): void
-    {
-        $teste = $this->configPaymentMethodsOffMock->isActive(null);
-        $this->assertEquals($teste, true);
+    public function test_isActive_get_true(){
+
+        $this->scopeConfigMock->expects($this->any())->method('getValue')
+        ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::ACTIVE))->willReturn(true);
+
+        $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+            $this->scopeConfigMock,
+            $this->dateMock,
+            $this->configMock,
+        );
+
+         $result = $configPaymentMethodsOff->isActive(null);
+
+        $this->assertTrue($result);
+    }
+
+    public function test_getTitle_get_with_value(){
+
+        $this->scopeConfigMock->expects($this->any())->method('getValue')
+        ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::TITLE))->willReturn('PaymentsOff');
+
+        $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+            $this->scopeConfigMock,
+            $this->dateMock,
+            $this->configMock,
+        );
+
+         $result = $configPaymentMethodsOff->getTitle(null);
+
+        $this->assertEquals('PaymentsOff', $result);
+    }
+
+    // public function test_getExpirationFormatted_get_with_value(){
+    //     $date = $dateMock->gmtDate('Y-m-d\T23:59:59.000O', strtotime("+2 days"));
+
+    //     $this->scopeConfigMock->expects($this->any())->method('getValue')
+    //     ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::EXPIRATION))->willReturn(2);
+
+    //     $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+    //         $this->scopeConfigMock,
+    //         $this->dateMock,
+    //         $this->configMock,
+    //     );
+
+    //      $result = $configPaymentMethodsOff->getExpirationFormatted(null);
+
+    //     $this->assertEquals($date, $result);
+    // }
+
+    public function test_hasUseDocumentIdentificationCapture_get_true(){
+
+        $this->scopeConfigMock->expects($this->any())->method('getValue')
+        ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::USE_GET_DOCUMENT_IDENTIFICATION))->willReturn(true);
+
+        $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+            $this->scopeConfigMock,
+            $this->dateMock,
+            $this->configMock,
+        );
+
+         $result = $configPaymentMethodsOff->hasUseDocumentIdentificationCapture(null);
+
+        $this->assertTrue($result);
+    }
+
+    public function test_hasUseNameCapture_get_true(){
+
+        $this->scopeConfigMock->expects($this->any())->method('getValue')
+        ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::USE_GET_NAME))->willReturn(true);
+
+        $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+            $this->scopeConfigMock,
+            $this->dateMock,
+            $this->configMock,
+        );
+
+         $result = $configPaymentMethodsOff->hasUseNameCapture(null);
+
+        $this->assertTrue($result);
+    }
+
+    public function test_getPaymentMethodsOffActive_get_value(){
+
+        $this->scopeConfigMock->expects($this->any())->method('getValue')
+        ->with(sprintf(self::PATH_PATTERN, ConfigPaymentMethodsOff::METHOD, ConfigPaymentMethodsOff::PAYMENT_METHODS))->willReturn('teste1,teste2');
+
+        $configPaymentMethodsOff = new ConfigPaymentMethodsOff(
+            $this->scopeConfigMock,
+            $this->dateMock,
+            $this->configMock,
+        );
+
+         $result = $configPaymentMethodsOff->getPaymentMethodsOffActive(null);
+
+         $this->assertEquals('teste1,teste2', $result);
     }
 }
