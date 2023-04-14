e48e4bae0dee0a56f71a3c146ad7563694bd3055
katelucena-meli <kate.lucena@mercadolivre.com>
2023-04-14 18:22:37 +0000

    Tweaked x-idempotency switch to create unique value

--- Gateway/Request/RefundRequest.php
+++ Gateway/Request/RefundRequest.php
@@ -93,16 +93,14 @@ class RefundRequest implements BuilderInterface
         $totalCreditmemo = $this->configPayment->formatPrice($creditmemo->getGrandTotal(), $storeId);
 
         $result = [
-            self::MERCADOPAGO_PAYMENT_ID => preg_replace('/[^0-9]/', '', $payment->getLastTransId()),
-            self::X_IDEMPOTENCY_KEY      => $payment->getLastTransId(),
+            self::MERCADOPAGO_PAYMENT_ID => preg_replace('/[^0-9]/', '', $payment->getTransactionId()),
+            self::X_IDEMPOTENCY_KEY      => $payment->getTransactionId() . '-' .  uniqid(),
         ];
 
         if ($grandTotal !== $totalCreditmemo) {
-            $result = [
-                self::MERCADOPAGO_PAYMENT_ID => preg_replace('/[^0-9]/', '', $payment->getTransactionId()),
-                self::X_IDEMPOTENCY_KEY      => $payment->getTransactionId(),
-                self::AMOUNT                 => $totalCreditmemo,
-            ];
+            $result = array_merge($result, [
+                self::AMOUNT => $totalCreditmemo,
+            ]);
         }
 
         return $result;
